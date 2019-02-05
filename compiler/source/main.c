c09311fd6ef736f6f0221ff45fe77a2c3fa5001a
Mads Kempf <madskempf@hotmail.com>
2019-02-05 14:24:23 +0100

    Update main.c
    det her var det værste rod nogensinde hahahahahaha

--- compiler/source/main.c
+++ compiler/source/main.c
@@ -30,29 +30,3 @@ int main(int argc, char** argv){
   dumpSymbolTable(childTable);
 
 }
-
-
-void createAFuckingTree(){
-  int tableCount = 20;
-  SymbolTable *tables[tableCount]; //list of pointers to any symbolTable in the tree
-  SymbolTable *tableRoot = initSymbolTable();
-  tables[0] = tableRoot;
-
-  //create tables
-  for(int i = 0; i < tableCount; tableCount++){
-    int search = 1; //continue to search for parent
-    int j = 1; //randomization factor for finding parent
-    int parentIndex = i*j*300 % HashSize;
-    while(tables[parentIndex] == NULL){
-      int parentIndex = i*j*300 % HashSize;
-    }
-    SymbolTable *childTable = scopeSymbolTable(tables[parentIndex]); //set parent of child
-    tables[i] = childTable; //save child i table
-  }
-
-  //insert symbol into the tables
-  for(int i = 0; i<500; i++){
-    
-  }
-
-}
