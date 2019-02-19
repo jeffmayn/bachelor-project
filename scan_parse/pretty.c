b9f00beab52c0d27bf2fd0b9e528617425a75f3a
Andreas Holm Jørgensen <SDU\andjo16@imada-106326.adm.c.sdu.dk>
2019-02-19 10:16:38 +0100

    pretty skeleton done

--- scan_parse/pretty.c
+++ scan_parse/pretty.c
@@ -17,3 +17,62 @@ void prettyEXP(EXP *e){
       break;
     }
 }
+
+//JEFF
+void pFUNC(FUNCTION *f){
+
+}
+void pHEAD(HEAD *h){
+
+}
+void pBODY(BODY *b){
+
+}
+void pTAIL(TAIL *t){
+
+}
+void pTYPE(TYPE *t){
+
+}
+//ANDREAS
+void pPARDECLLIST(PAR_DECL_LIST *pdl){
+
+}
+void pVARDECLLIST(VARDECLLIST *vdl){
+
+}
+void pVARTYPE(VAR_TYPE *vt){
+
+}
+void pDECLLIST(DECL_LIST *dl){
+
+}
+void pDECL(DECLARATION *d){
+
+}
+
+//FØRST TIL MØLLE
+void pSTMTLIST(STATEMENT_LIST *sl){
+
+}
+void pSTMT(STATEMENT *s){
+
+}
+
+
+//MADS
+void pVARIABLE(VARIABLE *v){
+
+}
+void pEXP(EXP *e){
+
+}
+void pTERM(TERM *t){
+
+}
+void pACTLIST(ACT_LIST *al){
+
+}
+void pEXPLIST(EXP_LIST *el){
+
+}
--- scan_parse/pretty.h
+++ scan_parse/pretty.h
@@ -1,3 +1,22 @@
 #include "tree.h"
 
 void prettyEXP(EXP *e);
+
+
+void pFUNC(FUNCTION *f);
+void pHEAD(HEAD *h);
+void pBODY(BODY *b);
+void pTAIL(TAIL *t);
+void pTYPE(TYPE *t);
+void pPARDECLLIST(PAR_DECL_LIST *pdl);
+void pVARDECLLIST(VARDECLLIST *vdl);
+void pVARTYPE(VAR_TYPE *vt);
+void pDECLLIST(DECL_LIST *dl);
+void pDECL(DECLARATION *d);
+void pSTMTLIST(STATEMENT_LIST *sl);
+void pSTMT(STATEMENT *s);
+void pVARIABLE(VARIABLE *v);
+void pEXP(EXP *e);
+void pTERM(TERM *t);
+void pACTLIST(ACT_LIST *al);
+void pEXPLIST(EXP_LIST *el);
