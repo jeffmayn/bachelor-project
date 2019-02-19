e0ba5139f0e27a3bd359390a46ed3767537a6e30
Jeff Gyldenbrand Skov Jørgensen <SDU\jegyl16@imada-106336.adm.c.sdu.dk>
2019-02-19 11:40:58 +0100

    pretty.c 1-5 plus sTMTLIST

--- scan_parse/pretty.c
+++ scan_parse/pretty.c
@@ -20,20 +20,68 @@ void prettyEXP(EXP *e){
 
 //JEFF
 void pFUNC(FUNCTION *f){
-
+  printf("(");
+  pHEAD(f->head);
+  INDENT += 2;
+  pBODY(f->body);
+  INDENT -= 2;
+  pTAIL(f->tail);
+  printf(")");
 }
+
 void pHEAD(HEAD *h){
-
+  printf("(");
+  printf("%s ", h->id);
+  pPARDECLLIST(h->pList);
+  printf(":");
+  pTYPE(h->type);
+  printf(")");
 }
+
 void pBODY(BODY *b){
-
+  printf("(");
+  pDECLLIST(b->vList);
+  printf(" ");
+  pSTMTLIST(b->sList);
+  printf(")");
 }
+
 void pTAIL(TAIL *t){
-
+  printf("(%s)", t->id);
 }
+
 void pTYPE(TYPE *t){
-
+  switch (t->kind) {
+    case idK:
+        printf("(");
+        printf("%s", t->val.id);
+        printf(")");
+        break;
+    case intK:
+        printf("(");
+        printf("int");
+        printf(")");
+        break;
+    case boolK:
+        printf("(");
+        printf("bool");
+        printf(")");
+        break;
+    case arrayK:
+        printf("(");
+        printf("array of ");
+        pTYPE(t->val.arrayType);
+        printf(")");
+        break;
+    case recordK:
+        printf("(");
+        printf("%record of ");
+        pVARDECLLIST(t->val.vList);
+        printf(")");
+        break;
+  }
 }
+
 //ANDREAS
 void pPARDECLLIST(PAR_DECL_LIST *pdl){
 
@@ -53,8 +101,10 @@ void pDECL(DECLARATION *d){
 
 //FØRST TIL MØLLE
 void pSTMTLIST(STATEMENT_LIST *sl){
-
+  printf("(%s)", sl->statement);
+  printf(" (%s)", sl->statementList);
 }
+
 void pSTMT(STATEMENT *s){
 
 }
