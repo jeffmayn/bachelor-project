76c9ee1b6ac80761f0c8dfa23147f757f87c4d10
Andreas <andjo16@student.sdu.dk>
2019-02-26 11:55:42 +0100

    lavede type branch

---
+++ typecheck/typecheck.h
@@ -0,0 +1,15 @@
+
+/**
+  Finds the types of all declared types
+*/
+void idTypeFinder();
+
+/**
+ * Finds the types of expressions
+*/
+void expTypeFinder();
+
+/**
+ * Checks if expression types match variable and context types
+*/
+void checkTypes();
