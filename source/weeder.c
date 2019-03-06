8d2f65adf9174fa2a6a3d301ed60802efa31c9e3
Jeff Gyldenbrand Skov Jørgensen <SDU\jegyl16@imada-106332.adm.c.sdu.dk>
2019-03-06 13:48:32 +0100

    jeg er forvirret

--- source/weeder.c
+++ source/weeder.c
@@ -5,44 +5,72 @@
 extern BODY *theexpression;
 
 void *weederBody(BODY *body){//TODO return -1 on error.
-  fprintf(stderr,"ENTERING weederBody\n");
+  //fprintf(stderr,"ENTERING weederBody\n");
   body = theexpression;
   traverseBody(body);
+  checkForReturn(body);
 }
 
 void *traverseBody(BODY *body){
-  fprintf(stderr,"ENTERING traverseBody\n");
+  //fprintf(stderr,"ENTERING traverseBody\n");
   traverseDECL(body->vList);
 }
 
 void *traverseDECL(DECL_LIST *decl){
-  fprintf(stderr,"ENTERING traverseDECL\n");
+  //fprintf(stderr,"ENTERING traverseDECL\n");
   if(decl != NULL){
     if (decl->decl != NULL && decl->decl->kind == 1){
       weederFunction(decl->decl->val.func);
-      fprintf(stderr," --> kind = function\n");
+    //  fprintf(stderr," --> kind = function\n");
     } else {
     //  fprintf(stderr," --> kind = %d\n", decl->decl->kind);
-
     }
     traverseDECL(decl->decl_list);
   }
 }
 
 FUNCTION *weederFunction(FUNCTION *f){//TODO return statement.
-  fprintf(stderr,"ENTERING weederFunction\n");
+  //fprintf(stderr,"ENTERING weederFunction\n");
   // for debugging
   fprintf(stderr,"function head-id: %s\n", f->head->id);
   fprintf(stderr,"function tail-id: %s\n", f->tail->id);
 
   // check header-id og tail-id er ens
   if ((strcmp (f->head->id, f->tail->id))==0 ){
-    fprintf(stderr,"same");
+    fprintf(stderr,"!! same !!\n\n");
     //return f;
   } else {
-    fprintf(stderr,"not same");
+    fprintf(stderr,"!! not same !!\n\n");
     // TODO: do stuff
   }
-
   traverseBody(f->body);
+}
+
+// NEW STUFF
+void *checkForReturn(BODY *body){
+  printf("ENTERING checkForReturn\n");
+  traverseSTMTlist(body->sList);
+}
+
+void *traverseSTMTlist(STATEMENT_LIST *stmtList){
+  printf("ENTERING traverseSTMTlist\n");
+  if(stmtList != NULL){
+    if(stmtList->statement->kind == 5){
+      ifthenelse(stmtList->statement->val.ifthenelse);
+    }
+    //printf("----> %d\n", stmtList->statement->val.assign.exp->kind);
+
+    //if(stmtList->statement != NULL && stmtList->statement->kind == 5){
+      printf("----> 5\n");
+    }
+
+  }
+
+void *ifthenelse(){
+  if ()
+
+}
+
+void *ifthen(){
+
 }
--- source/weeder.h
+++ source/weeder.h
@@ -3,11 +3,13 @@
 #include "tree.h"
 
 void *weederBody(BODY *body);
-
 FUNCTION *weederFunction(FUNCTION *f);
-
 void *traverseBody(BODY *body);
 void *traverseDECL(DECL_LIST *decl);
 
+void *checkForReturn(BODY *body);
+void *traverseSTMTlist(STATEMENT_LIST *stmtList);
 
+void *ifthenelse();
+void *ifthen();
 #endif
