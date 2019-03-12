71e70b7f5a33b37ee7ddc6974c7d16bbb0a629ef
jeffmayn <jeff@gyldenbrand.dk>
2019-03-12 14:22:37 +0100

    det virker nu.. 4realzgit branch

--- source/weeder.c
+++ source/weeder.c
@@ -16,7 +16,7 @@ int *weederBody(BODY *body){//TODO return -1 on error.
 int *traverseBody(BODY *body){
   int retVal = -2;
   retVal = traverseDECL(body->vList);
-  printf("traverseBody return: %d\n", retVal);
+//  printf("traverseBody return: %d\n", retVal);
   return retVal;
 }
 
@@ -28,49 +28,45 @@ int *traverseDECL(DECL_LIST *decl){
       if(retVal == -1){
         return -1;
       }
-
-      printf("traverseDECL return: %d\n", retVal);
+//      printf("traverseDECL return: %d\n", retVal);
     }
     return traverseDECL(decl->decl_list);
-    //return traverseDECL(decl->decl_list);
   }
 }
 
 int *weederFunction(FUNCTION *f){//TODO return statement.
   int retVal = -2;
   int retVal2 = -2;
-  //int retVal3 = -2;
 //  fprintf(stderr, " >> CHECK FOR HEAD-TAIL ID\n");
-  fprintf(stderr,"\nfunction: %s\n", f->head->id);
+//  fprintf(stderr,"\nfunction: %s", f->head->id);
 //  fprintf(stderr,"function tail-id: %s\n", f->tail->id);
   if ((strcmp (f->head->id, f->tail->id))==0 ){
-    fprintf(stderr," --> id return-code: 0\n");
-    return 0;
+//    fprintf(stderr," --> id return-code: 0\n");
+    retVal = 0;
   } else {
+  //  fprintf(stderr," --> id return-code: -1\n");
     fprintf(stderr, "Line %d: mismatch in header-id and tail-id\n", f->head->lineno);
     return -1;
+  //  return -1;
   }
 
   retVal = traverseBody(f->body);
   retVal2 = travCheckForReturn(f->body);
 
-  fprintf(stderr, "--> retVal2: %d\n", retVal2);
+//  fprintf(stderr, "--> retVal1: %d\n", retVal);
+//  fprintf(stderr, "--> retVal2: %d\n", retVal2);
 
-
-  if(retVal == -1 || retVal2 == -1){
-  //  fprintf(stderr, "Line %d: error\n", f->head->lineno);
-    return -1;
-  } else {
-    return 0;
-  }
 
 }
 
 // NEW STUFF
 int *travCheckForReturn(BODY *body){
-  fprintf(stderr, " >>> enters travCheckForReturn\n");
+//  fprintf(stderr, " >>> enters travCheckForReturn\n");
   int result = expTravStmts(body->sList);
-  fprintf(stderr, "end value: %d\n", result);
+  if(result == -1){
+    fprintf(stderr, "Line %d: missing return statement\n", body->lineno);
+  }
+//  fprintf(stderr, "end value: %d\n", result);
   return result;
 }
 
@@ -82,11 +78,11 @@ int *expTravStmts(STATEMENT_LIST *stmtList){
       }
       return 0;
     }
+    return expTravStmts(stmtList->statementList);
   }
   else{
     return -1;
   }
-  return expTravStmts(stmtList->statementList);
 }
 
 int expTravStmt(STATEMENT *s){
@@ -98,11 +94,31 @@ int expTravStmt(STATEMENT *s){
     case ifK:
       break;
     case thenK:
-      retVal = expTravStmts(s->val.ifthenelse.thenbody->val.list);
-      if(retVal == 0){
-        return expTravStmts(s->val.ifthenelse.elsebody->val.list);
+      if(s->val.ifthenelse.thenbody->kind == returnK){
+        return 0;
       }
-      fprintf(stderr, "Line %d: missing return statement\n", s->lineno);
+      else{
+        if(s->val.ifthenelse.thenbody->kind == listStmtK){
+          retVal = expTravStmts(s->val.ifthenelse.thenbody->val.list);
+        }
+        else{
+          return -1;
+        }
+      }
+      if(retVal == 0){
+        if(s->val.ifthenelse.elsebody->kind == returnK){
+          return 0;
+        }
+        else{
+          if(s->val.ifthenelse.elsebody->kind == listStmtK){
+            return expTravStmts(s->val.ifthenelse.elsebody->val.list);
+
+          } else {
+            return -1;
+          }
+        }
+      }
+      //fprintf(stderr, "Line %d: missing return statement\n", s->lineno);
       return -1;
       break;
     default:
