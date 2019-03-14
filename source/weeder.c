d4ef97bb66cf4ca515c5347c4af0d1e70959293f
Andreas Holm Jørgensen <SDU\andjo16@imada-106312.adm.c.sdu.dk>
2019-03-14 14:29:33 +0100

    Master seems to work - in some ways

--- source/weeder.c
+++ source/weeder.c
@@ -5,7 +5,7 @@
 
 extern BODY *theexpression;
 
-int *weederBody(BODY *body){//TODO return -1 on error.
+int weederBody(BODY *body){//TODO return -1 on error.
   int retVal = -2;
   body = theexpression;
   retVal = traverseBody(body);
@@ -13,13 +13,13 @@ int *weederBody(BODY *body){//TODO return -1 on error.
   return retVal;
 }
 
-int *traverseBody(BODY *body){
+int traverseBody(BODY *body){
   int retVal = -2;
   retVal = traverseDECL(body->vList);
   return retVal;
 }
 
-int *traverseDECL(DECL_LIST *decl){
+int traverseDECL(DECL_LIST *decl){
   int retVal = -2;
   if(decl != NULL){                    // kind 1 = function
     if (decl->decl != NULL && decl->decl->kind == 1){
@@ -32,7 +32,7 @@ int *traverseDECL(DECL_LIST *decl){
   }
 }
 
-int *weederFunction(FUNCTION *f){
+int weederFunction(FUNCTION *f){
   int retVal = -2;
   int retVal2 = -2;
   if ((strcmp (f->head->id, f->tail->id))==0 ){
@@ -46,7 +46,7 @@ int *weederFunction(FUNCTION *f){
 }
 
 // NEW STUFF
-int *travCheckForReturn(BODY *body){
+int travCheckForReturn(BODY *body){
   int result = expTravStmts(body->sList);
   if(result == -1){
     fprintf(stderr, "Error! Line %d: missing return statement\n", body->lineno);
@@ -54,7 +54,7 @@ int *travCheckForReturn(BODY *body){
   return result;
 }
 
-int *expTravStmts(STATEMENT_LIST *stmtList){
+int expTravStmts(STATEMENT_LIST *stmtList){
   if(stmtList != NULL){
     if(expTravStmt(stmtList->statement) == 0){
       if(stmtList->statementList != NULL){
--- source/weeder.h
+++ source/weeder.h
@@ -2,15 +2,15 @@
 #define __weeder_h
 #include "tree.h"
 
-int *weederBody(BODY *body);
-int *weederFunction(FUNCTION *f);
-int *traverseBody(BODY *body);
-int *traverseDECL(DECL_LIST *decl);
+int weederBody(BODY *body);
+int weederFunction(FUNCTION *f);
+int traverseBody(BODY *body);
+int traverseDECL(DECL_LIST *decl);
 
 int checkForReturn(STATEMENT_LIST *sList);
 
-int *expTravStmts(STATEMENT_LIST *stmtList);
+int expTravStmts(STATEMENT_LIST *stmtList);
 int expTravStmt(STATEMENT *s);
 
-int *travCheckForReturn(BODY *body);
+int travCheckForReturn(BODY *body);
 #endif
