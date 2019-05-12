2a308728e40941e4b20480f35db80b526abae8aa
Andreas <andjo16@student.sdu.dk>
2019-05-12 22:59:06 +0200

    Løst de fleste problemer - mangler livenessproblemet

--- compiler/blah.sh
+++ compiler/blah.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-for file in test/unitTests/ArrType*.kit; do
+for file in test/unitTests/FuncAnonRec.kit; do
   echo "########" $file
   ./build/compiler < $file > a.s &&
   gcc -no-pie a.s &&
   file=${file##*/} &&
-  ./a.out > test/unitTests/tmp/expected/${file%.kit}.txt
-  #./a.out
-  #echo ret $?
+  #./a.out > test/unitTests/tmp/expected/${file%.kit}.txt
+  ./a.out
+  echo ret $?
 done
