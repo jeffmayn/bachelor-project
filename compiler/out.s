bfbb5abb2732db5d6f4c5bbe1656c20cf6fd88df
Jeff Gyldenbrand Skov Jørgensen <SDU\jegyl16@imada-106311.adm.c.sdu.dk>
2019-05-13 12:03:42 +0200

    løkker og if-sætninger næsten færdig

--- compiler/out.s
+++ compiler/out.s
@@ -22,30 +22,201 @@ main:
 	push %r13
 	push %r14
 	push %r15
-	subq $16, %rsp
+	subq $208, %rsp
 	movq %rsp, mainSPoint4
 	movq %rbp, mainBPoint5
 	movq $heap1, freeHeap2
 	movq $heap1, endHeap3
 	addq $1048576, endHeap3
-#line: 1 write statement
-	movq $1, %r9
-	movq $12, %rbx
+	movq $0, %rbx
+	movq %rbp, %rdi#resetting basepointer
 	movq %rbx, %r8
-	movq $12, %rbx
+	push %rbx
+	movq %rbp, %rbx
+	movq %rbx, %r9
+	movq %r9, %rbx
+	movq %rbx, %rdi
+	pop %rbx
+	movq %r8, %rbx
+	movq $-6, %rdx
+	movq %rbx, (%rdi,%rdx,8)
+	movq %rbp, %rdi#resetting basepointer
+	movq $666, %rbx
+	movq %rbp, %rdi#resetting basepointer
+	movq %rbx, %r8
+	push %rbx
+	movq %rbp, %rbx
+	movq %rbx, %r9
+	movq %r9, %rbx
+	movq %rbx, %rdi
+	pop %rbx
+	movq %r8, %rbx
+	movq $-7, %rdx
+	movq %rbx, (%rdi,%rdx,8)
+	movq %rbp, %rdi#resetting basepointer
+while8:
+	movq $1, %r9
+	push %rbx
+	movq %rbp, %rbx
+	movq %rbx, %r8
+	movq %r8, %rbx
+	movq %rbx, %rdi
+	pop %rbx
+	movq $-6, %rdx
+	movq (%rdi,%rdx,8), %rbx
+	movq %rbp, %rdi#resetting basepointer
+	movq %rbx, %r8
+	movq $10, %rbx
+	movq %rbp, %rdi#resetting basepointer
 	cmp %rbx, %r8
-	jle ge8
+	jl ge9
 	movq $0, %r9
-ge8:
+ge9:
+	movq %r9, %rbx
+	cmp $1, %rbx
+	jne endwhile8
+	push %rbx
+	movq %rbp, %rbx
+	movq %rbx, %r8
+	movq %r8, %rbx
+	movq %rbx, %rdi
+	pop %rbx
 	push %rdi
-	movq %r9, %rsi
+	movq $-6, %rdx
+	movq (%rdi,%rdx,8), %rsi
 	movq $format, %rdi
 	movq $0, %rax
 	call printf
 	pop %rdi
+	push %rbx
+	movq %rbp, %rbx
+	movq %rbx, %r8
+	movq %r8, %rbx
+	movq %rbx, %rdi
+	pop %rbx
+	movq $-6, %rdx
+	movq (%rdi,%rdx,8), %rbx
+	movq %rbp, %rdi#resetting basepointer
+	movq %rbx, %r8
+	movq $1, %rbx
+	movq %rbp, %rdi#resetting basepointer
+	addq %rbx, %r8
+	movq %r8, %rbx
+	movq %rbp, %rdi#resetting basepointer
+	movq %rbx, %r8
+	push %rbx
+	movq %rbp, %rbx
+	movq %rbx, %r9
+	movq %r9, %rbx
+	movq %rbx, %rdi
+	pop %rbx
+	movq %r8, %rbx
+	movq $-6, %rdx
+	movq %rbx, (%rdi,%rdx,8)
+	movq %rbp, %rdi#resetting basepointer
+	movq $1, %r9
+	push %rbx
+	movq %rbp, %rbx
+	movq %rbx, %r8
+	movq %r8, %rbx
+	movq %rbx, %rdi
+	pop %rbx
+	movq $-6, %rdx
+	movq (%rdi,%rdx,8), %rbx
+	movq %rbp, %rdi#resetting basepointer
+	movq %rbx, %r8
+	movq $7, %rbx
+	movq %rbp, %rdi#resetting basepointer
+	cmp %rbx, %r8
+	jl ge11
+	movq $0, %r9
+ge11:
+	movq %r9, %rbx
+	movq %rbp, %rdi#resetting basepointer
+	cmp $1, %rbx
+	jne endif10
+	push %rbx
+	movq %rbp, %rbx
+	movq %rbx, %r8
+	movq %r8, %rbx
+	movq %rbx, %rdi
+	pop %rbx
+	push %rdi
+	movq $-7, %rdx
+	movq (%rdi,%rdx,8), %rsi
+	movq $format, %rdi
+	movq $0, %rax
+	call printf
+	pop %rdi
+while12:
+	movq $1, %r8
+	push %rbx
+	movq %rbp, %rbx
+	movq %rbx, %r9
+	movq %r9, %rbx
+	movq %rbx, %rdi
+	pop %rbx
+	movq $-7, %rdx
+	movq (%rdi,%rdx,8), %rbx
+	movq %rbp, %rdi#resetting basepointer
+	movq %rbx, %r9
+	movq $666, %rbx
+	movq %rbp, %rdi#resetting basepointer
+	cmp %rbx, %r9
+	je eq13
+	movq $0, %r8
+eq13:
+	movq %r8, %rbx
+	cmp $1, %rbx
+	jne endwhile12
+	push %rbx
+	movq %rbp, %rbx
+	movq %rbx, %r8
+	movq %r8, %rbx
+	movq %rbx, %rdi
+	pop %rbx
+	push %rdi
+	movq $-7, %rdx
+	movq (%rdi,%rdx,8), %rsi
+	movq $format, %rdi
+	movq $0, %rax
+	call printf
+	pop %rdi
+	push %rbx
+	movq %rbp, %rbx
+	movq %rbx, %r8
+	movq %r8, %rbx
+	movq %rbx, %rdi
+	pop %rbx
+	movq $-7, %rdx
+	movq (%rdi,%rdx,8), %rbx
+	movq %rbp, %rdi#resetting basepointer
+	movq %rbx, %r8
+	movq $1, %rbx
+	movq %rbp, %rdi#resetting basepointer
+	subq %rbx, %r8
+	movq %r8, %rbx
+	movq %rbp, %rdi#resetting basepointer
+	movq %rbx, %r8
+	push %rbx
+	movq %rbp, %rbx
+	movq %rbx, %r9
+	movq %r9, %rbx
+	movq %rbx, %rdi
+	pop %rbx
+	movq %r8, %rbx
+	movq $-7, %rdx
+	movq %rbx, (%rdi,%rdx,8)
+	movq %rbp, %rdi#resetting basepointer
+	jmp while12
+	jmp while12
+endwhile12:
+endif10:
+	jmp while8
+endwhile8:
 	movq $0, %rax
 mainend:
-	addq $16, %rsp
+	addq $208, %rsp
 	pop %r15
 	pop %r14
 	pop %r13
